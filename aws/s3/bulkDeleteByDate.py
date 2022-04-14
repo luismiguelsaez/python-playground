3cf84577868d3263a0225b96fbe4fcb99890d4e6
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-04-14 16:41:15 +0200

    Added output file

--- aws/s3/bulkDeleteByDate.py
+++ aws/s3/bulkDeleteByDate.py
@@ -8,6 +8,7 @@ parser.add_argument('--bucket', required=True, action='store', help='Name of the
 parser.add_argument('--prefix', required=True, action='store', help='Object name prefix')
 parser.add_argument('--modified-before', required=True, action='store', help='Objects where created before this date ( %Y-%m-%d %H:%M:%S )')
 parser.add_argument('--modified-after', required=True, action='store', help='Objects where created after this date ( %Y-%m-%d %H:%M:%S )')
+parser.add_argument('--output', required=False, action='store', help='Optional file to write the deleted objects')
 
 args = vars(parser.parse_args())
 
@@ -15,21 +16,36 @@ bucket = args["bucket"]
 namePrefix = args["prefix"]
 modBefore = args["modified_before"]
 modAfter  = args["modified_after"]
-
+outputEnabled = False
 
 s3 = boto3.client("s3")
 
 paginator = s3.get_paginator("list_objects_v2")
-pages = paginator.paginate(Bucket=bucket)
+paginator_params = {'Bucket': bucket, 'Prefix': namePrefix}
+pages = paginator.paginate(**paginator_params)
+
+if "output" in args:
+    outputFile = open("demofile2.txt", "a")
+    outputEnabled = True
+
 dateBefore = datetime.utcnow().strptime(modBefore, '%Y-%m-%d %H:%M:%S')
 dateAfter = datetime.utcnow().strptime(modAfter, '%Y-%m-%d %H:%M:%S')
 
-keys_to_delete = []
+totalSize = 0
+deletedFiles = 0
+keysToDelete = []
+
 for page in pages:
     for object in page["Contents"]:
         if object["LastModified"] > dateAfter.replace(tzinfo=timezone.utc) and object["LastModified"] < dateBefore.replace(tzinfo=timezone.utc):
-            if re.search("^{}".format(namePrefix), object["Key"]):
-                print("Found object with date [{}]: {}".format(object["LastModified"], object["Key"]))
-                keys_to_delete.append({"Key": object["Key"]})
+                s3.delete_objects(Bucket=bucket, Key=object["Key"])
+                if outputEnabled:
+                    outputFile.write("{},{},{}".format(object["Key"],object["LastModified"],object["Size"]))
+                totalSize += object["Size"]
+                deletedFiles += 1
+                keysToDelete.append({"Key": object["Key"]})
 
-#s3.delete_objects(Bucket=bucket, Delete={"Objects": keys_to_delete})
+print("Total size: {} bytes", totalSize)
+#s3.delete_objects(Bucket=bucket, Delete={"Objects": keysToDelete})
+
+outputFile.close()
