e0f7b218d8e3983ad0d90c9cbbf7eefd2fa5a5dc
luismiguel.saez <luismiguelsaez83@gmail.com>
2022-06-24 10:01:03 +0200

    Modified metrics names

--- api/flask/code/main.py
+++ api/flask/code/main.py
@@ -9,29 +9,39 @@ logging.basicConfig(level=logging.INFO)
 logging.info("Setting LOGLEVEL to INFO")
 
 api = Flask(__name__)
-metrics = PrometheusMetrics(api)
 
+metrics = PrometheusMetrics(api, group_by='endpoint')
 metrics.info("app_info", "", version="1.0.0")
 
 byPathCounter = metrics.counter(
-    'byPathCounter', 'Request count by request paths',
+    'api_requests_count_by_path', 'Request count by request paths',
     labels={'path': lambda: request.path}
+)
+byStatusSummary = metrics.summary(
+  'api_requests_latency_by_status', 'Request latencies by status',
+  labels={'status': lambda r: r.status_code}
+)
+byStatusPathHistogram = metrics.histogram(
+  'api_requests_latency_by_status_and_path', 'Request latencies by status and path',
+  labels={'status': lambda r: r.status_code, 'path': lambda: request.path}
 )
 
 @api.route("/health",methods=['GET'])
-@byPathCounter
+@metrics.do_not_track()
 def health():
   return jsonify({"healthy":"true"}), 200
 
 @api.route("/",methods=['GET'])
 @byPathCounter
+@byStatusSummary
+@byStatusPathHistogram
 def root():
   msg, httpCode = randomResponse()
   return jsonify(msg), httpCode
 
 def randomResponse():
   httpCodes = [200,500]
-  httpResponseCode = random.choices(population=httpCodes,weights=(95,10),k=1)
+  httpResponseCode = random.choices(population=httpCodes,weights=(5,95),k=1)
   if httpResponseCode[0] == 200:
     return {"message": "success"}, httpResponseCode[0]
   elif httpResponseCode[0] == 500:
