2edaf9953b0bd6e4cfcf797ea7c9d02d61fbb50a
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-11-08 19:30:13 +0100

    Added keyboard error detection

--- algorithms/text/match.py
+++ algorithms/text/match.py
@@ -6,7 +6,7 @@ def split_text(text: str, length: int)->list:
   return list(filter(lambda x: len(x) == length, strings))
 
 
-def find_in_text(text: str, sub: str, case=True)->bool:
+def find_in_text(text: str, sub: str, case=False)->bool:
 
   if not case:
     sub = sub.lower()
@@ -18,15 +18,38 @@ def find_in_text(text: str, sub: str, case=True)->bool:
     return False
 
 
+def keyboard_detect_error(k: str)->list:
+
+  keys = [
+    "QWERTYUIOP",
+    "ASDFGHJKL",
+    "ZXCVBNM"
+  ]
+
+  keys_group = []
+  for i in range(len(keys)):
+    for j in range(len(keys[i])):
+      if keys[i][j] == k:
+        keys_group += keys[i][j:j+2]
+        keys_group += keys[i][j-1:j]
+
+  return keys_group
+
+
 def main():
 
-  test_text = "Hotel transilvania"
-  test_subs = "Trans"
+  hotel_names = [
+    "Grand Transilvania",
+    "Obscure Castle of Illusion",
+    "Hollow Casquet of Hell"
+  ]
 
-  if find_in_text(test_text, test_subs, case=False):
-    print("Found substring '{}' in text '{}'".format(test_subs, test_text))
-  else:
-    print("KO")
+  test_subs = "castle"
 
+  for hotel_name in hotel_names:
+    if find_in_text(hotel_name, test_subs):
+      print("Found substring '{}' in text '{}'".format(test_subs, hotel_name))
+
+  print(keyboard_detect_error("G"))
 
 main()
