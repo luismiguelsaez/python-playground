af2ecd9c3748bbff963417e423c67849ac022759
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-11-11 20:29:15 +0100

    Optimized column flip

--- algorithms/matrix/flip.py
+++ algorithms/matrix/flip.py
@@ -10,13 +10,14 @@ def flip_row(matrix: list[list], row: int)->list[list]:
 
 def flip_col(matrix: list[list], col: int)->list[list]:
 
-  column = []
-  for i in range(len(matrix)):
-    column.append(matrix[i][col])
-  
-  column = column[::-1]
-  for i in range(len(matrix)):
-    matrix[i][col] = column[i]
+  max = len(matrix)
+  for i in range(max//2):
+    matrix[i][col],matrix[max-i-1][col] = matrix[max-i-1][col],matrix[i][col]
+
+  #column = [matrix[i][col] for i in range(len(matrix))]
+  #column = column[::-1]
+  #for i in range(len(matrix)):
+  #  matrix[i][col] = column[i]
 
   return matrix
 
@@ -44,14 +45,14 @@ def main():
   for i in range(len(test_matrix)):
     for j in range(len(test_matrix)):
       tmp_matrix = deepcopy(test_matrix)
-      tmp_matrix = flip_row(tmp_matrix, i)
-      tmp_matrix = flip_col(tmp_matrix, j)
+      tmp_matrix = flip_col(tmp_matrix, i)
+      tmp_matrix = flip_row(tmp_matrix, j)
       tmp_sum = sum_quadrant(tmp_matrix)
       if tmp_sum > max:
         max = tmp_sum
 
       print()
-      print("- Flipped row:",i,"col:",j)
+      print("- Flipped col:",i,"row:",j)
       for r in range(len(tmp_matrix)):
         print(*tmp_matrix[r])
       print("- Sum:", tmp_sum)
