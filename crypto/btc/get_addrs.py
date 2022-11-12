dd96ee55e3b62b1b9e7ac49da5b021c6ee9860e6
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-11-12 11:47:49 +0100

    Added btc

---
+++ crypto/btc/get_addrs.py
@@ -0,0 +1,18 @@
+import pandas
+from sys import argv
+
+base_addr = 'https://blockchain.info/rawaddr/'
+btc_addrs = [x for x in argv[1:]]
+
+sum = 0
+for addr in btc_addrs:
+  df = pandas.read_json(base_addr + addr)
+  transactions = df['txs']
+
+  for transaction in transactions:
+    n = list(filter(lambda x:x['addr'] == addr, transaction['out']))
+    for t in n:
+      sum += t['value']/100000000
+      print("Address", addr, t['value']/100000000)
+
+print("Total", sum)
