fd418dcda3ba011e1a475d36da7900305bf765f5
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-11-13 11:13:27 +0100

    Added logging

--- aws/sam/rds/ecr-repo-autocreate/src/app.py
+++ aws/sam/rds/ecr-repo-autocreate/src/app.py
@@ -1,41 +1,51 @@
 import boto3
+import logging
+from sys import stdout
 
 def lambda_handler(event, context):
+
+    logger = logging.getLogger("custom")
+    h_stdout = logging.StreamHandler(stdout)
+    f_stdout = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    h_stdout.setFormatter(f_stdout)
+    logger.addHandler(h_stdout)
+    logger.setLevel(logging.DEBUG)
 
     session = boto3.Session()
 
     client_ecr = session.client("ecr")
 
-    print("Event:", str(event))
+    logger.debug("Event: {}".format(str(event)))
 
     if event["detail"]["errorCode"] == "RepositoryNotFoundException":
         try:
             ecr_response = client_ecr.describe_repositories()
             repo_name = event["detail"]["requestParameters"]["repositoryName"]
-    
-            print(event)
 
             if len(list(filter(lambda x: x['repositoryName'] == repo_name, ecr_response['repositories']))) > 0:
                 response = client_ecr.create_repository(
-                repositoryName=repo_name,
-                tags=[
-                    {
-                    'Key':'auto-create',
-                    'Value':'true'
+                    repositoryName=repo_name,
+                    tags=[
+                        {
+                        'Key':'auto-create',
+                        'Value':'true'
+                        }
+                    ],
+                    imageScanningConfiguration={
+                        'scanOnPush': True
+                    },
+                    encryptionConfiguration={
+                        'encryptionType': 'AES256'
                     }
-                ],
-                imageScanningConfiguration={
-                    'scanOnPush': True
-                },
-                encryptionConfiguration={
-                    'encryptionType': 'AES256'
-                }
                 )
         except boto3.RepositoryAlreadyExistsException as repoException:
+            logger.error("Repository [{}] already exists:{}".format(repo_name,repoException))
             return { 'message' : "Repository [{}] already exists:{}".format(repo_name,repoException) }
         except Exception as exception:
+            logger.error("Error while creating repository [{}]: {}")
             return { 'message' : "Error while creating repository [{}]: {}".format(repo_name,exception) }
         else:
+            logger.info("Repository [{}] created".format(repo_name))
             return { 'message': "Repository [{}] created".format(repo_name) }
 
 
