e326a8f1c9424cdc23956ca61d8bc6f58a49dc5c
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-11-14 17:32:41 +0100

    Modified date format

--- crypto/btc/get_addrs.py
+++ crypto/btc/get_addrs.py
@@ -1,5 +1,6 @@
 import pandas
 from sys import argv
+import datetime
 
 base_addr = 'https://blockchain.info/rawaddr/'
 btc_addrs = [x for x in argv[1:]]
@@ -11,8 +12,10 @@ for addr in btc_addrs:
 
   for transaction in transactions:
     n = list(filter(lambda x:x['addr'] == addr, transaction['out']))
+    t_time = transaction['time']
+    t_datetime = datetime.datetime.fromtimestamp(t_time)
     for t in n:
       sum += t['value']/100000000
-      print("Address", addr, t['value']/100000000, "BTC")
+      print(f"Address {addr} - {t_datetime} - {t['value']/100000000} BTC")
 
 print("Total", sum, "BTC")
