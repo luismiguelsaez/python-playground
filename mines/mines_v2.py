4fe5d4f48b4618183649972424b2a78abc720aee
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-11-23 21:05:54 +0100

    Added mine detection

--- mines/mines_v2.py
+++ mines/mines_v2.py
@@ -10,6 +10,7 @@ def place_mines(panel: list, num: int, content: str='o')->list:
   s = len(panel)
 
   combinations = [(i, j) for i in range(s) for j in range(s)]
+  mines = []
 
   for _ in range(num):
     z = randrange(len(combinations))
@@ -17,14 +18,34 @@ def place_mines(panel: list, num: int, content: str='o')->list:
     y = combinations[z][1]
     panel[x][y] = content
     del combinations[z]
+    mines.append((x, y))
 
   return panel
 
+def place_numbers(panel: list, content: str='o')->list:
+
+  corners = [(0, 0), (len(panel)-1, len(panel)-1), (0, len(panel)-1), (len(panel)-1, 0)]
+
+  for i in range(len(panel)):
+    for j in range(len(panel)):
+      if panel[i][j] != content:
+        if (i, j) in corners:
+          print(f"Corner {i} {j} not a mine")
+        elif i == 0 or j == 0 or i == len(panel)-1 or j == len(panel)-1:
+          print(f"Lateral {i} {j} not a mine")
+        else:
+          print(f"{i} {j} not a mine")
+
+  return []
+
 def main():
-  test_panel = create_panel(size=4) 
-  place_mines(panel=test_panel, num=8)
+  test_panel = create_panel(size=6) 
+  place_mines(panel=test_panel, num=6)
 
   for i in test_panel:
     print(*i)
+
+  print()
+  place_numbers(test_panel)
 
 main()
