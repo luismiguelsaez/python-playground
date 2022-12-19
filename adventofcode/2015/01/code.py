de480eb78bfc5f636e9b4c4db3ff91b060bfb714
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-12-19 11:08:03 +0100

    Update

--- adventofcode/2015/01/code.py
+++ adventofcode/2015/01/code.py
@@ -6,11 +6,9 @@ print(f"Part 1: {floor}")
 # Result: 74
 
 floor = 0
-c = 0
-for f in [x.strip() for x in open('input.txt').readlines()][0]:
-  c += 1
+for c, f in enumerate([x.strip() for x in open('input.txt').readlines()][0]):
   floor += 1 if f == '(' else -1
   if floor == -1:
-    print(f"Part 2: {c}")
+    print(f"Part 2: {c+1}")
     # Result: 1795
     break
