359c36145471dbe41c76dee190ace75879f1af66
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-12-19 11:06:39 +0100

    Updated AoC 201501

--- adventofcode/2015/01/code.py
+++ adventofcode/2015/01/code.py
@@ -3,3 +3,14 @@ floor = 0
 for f in [x.strip() for x in open('input.txt').readlines()][0]: floor += 1 if f == '(' else -1
 
 print(f"Part 1: {floor}")
+# Result: 74
+
+floor = 0
+c = 0
+for f in [x.strip() for x in open('input.txt').readlines()][0]:
+  c += 1
+  floor += 1 if f == '(' else -1
+  if floor == -1:
+    print(f"Part 2: {c}")
+    # Result: 1795
+    break
