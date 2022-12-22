17c3fcb49b334713873c689fb97c17700cede115
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2022-12-22 12:05:54 +0100

    Simplified code

--- adventofcode/2022/10/code.py
+++ adventofcode/2022/10/code.py
@@ -7,6 +7,9 @@ def main():
   register = 1
   cycle = 0
   cycle_time = 0.01
+  pixel_pos = 0
+  rows = [[] for _ in range(6)]
+  row = 0
 
   for i in range(len(data)):
 
@@ -14,21 +17,39 @@ def main():
       for _ in range(1):
         sleep(cycle_time)
         cycle += 1
+        pixel_pos = cycle-1
+        row = pixel_pos//40
+        pixel_in_row = pixel_pos - (row*40)
+        rows[row] = ['.' for _ in range(40)]
+        for i in range(len(rows[row])):
+          if i == pixel_in_row:
+            rows[row][i] = '#'
+        print(*rows[row])
+        #print(f"[{cycle}] Pixel position {pixel_pos} row: {row}")
         if cycle == 20 or ((cycle - 20) % 40) == 0:
           signal_strength = cycle*register
           signal_strength_total += signal_strength
-          print(f"[{cycle}] reg: {register} signal strength: {signal_strength}")
+          #print(f"[{cycle}] reg: {register} signal strength: {signal_strength}")
     else:
       inc = int(data[i].split()[1])
       for _ in range(2):
         sleep(cycle_time)
         cycle += 1
+        pixel_pos = cycle-1
+        row = pixel_pos//40
+        pixel_in_row = pixel_pos - (row*40)
+        rows[row] = ['.' for _ in range(40)]
+        for i in range(len(rows[row])):
+          if i == pixel_in_row:
+            rows[row][i] = '#'
+        print(*rows[row])
+        #print(f"[{cycle}] Pixel position {pixel_pos} row: {row}")
         if cycle == 20 or ((cycle - 20) % 40) == 0:
           signal_strength = cycle*register
           signal_strength_total += signal_strength
-          print(f"[{cycle}] reg: {register} signal strength: {signal_strength}")
+          #print(f"[{cycle}] reg: {register} signal strength: {signal_strength}")
       register += inc
 
-  print(f"Signal strength total: {signal_strength_total}")
+  print(f"Part 1: signal strength total: {signal_strength_total}")
 
 main()
