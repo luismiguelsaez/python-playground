c6947c24c783cb222bb8f348ecd08cc3862e40b4
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-02-18 12:37:03 +0100

    Updated algo

--- algorithms/ratelimit/tokenBucket.py
+++ algorithms/ratelimit/tokenBucket.py
@@ -1,21 +1,26 @@
 
-input_data = [1,1,1,1,1,2,2,2,3,3,3,4,4,4,4,6,6,6,7,7,7,7,8,8,9,9,9,9,9,9,10,10,10,11,11,11,11,12,12,12,13,13]
+input_data = [1,1,1,1,1,2,2,2,4,4,4,4,4,6,6,6,8,8,8,8,8,9,9,9,9,9,9,10,10,10,11,11,11,11,12,12,12,13,13]
 
 buckets = {
-  1: { "max": 4, "curr": 4, "val": 0 },
-  5: { "max": 10, "curr": 4, "val": 0 }
+  1: { "max": 4,  "cur": 4,  "t_ini": 0 },
+  5: { "max": 10, "cur": 10, "t_ini": 0 }
 }
 
-
 for i in range(len(input_data)):
+  
+  for b in buckets:
+    if i == 0: buckets[b]['t_ini'] = input_data[i]
 
-  for bucket,val in buckets.items():
-    if input_data[i] % bucket == 0 and buckets[bucket]['val'] != input_data[i]:
-      buckets[bucket]['val'] = input_data[i]
-      buckets[bucket]['curr'] = buckets[bucket]['max']
-    buckets[bucket]['curr'] -= 1
+    if input_data[i] - buckets[b]['t_ini'] >= b:
+      buckets[b]['cur'] = buckets[b]['max']
+      buckets[b]['t_ini'] = input_data[i]
+    
+    buckets[b]['cur'] -= 1
 
-  buckets_status = [x for x,y in buckets.items() if y['curr'] < 0]
-  res = "reject" if len(buckets_status) > 0 else "pass"
+    if buckets[b]['cur'] < 0:
+      res = 'drop'
+      break
+    else:
+      res = 'forward'
 
-  print(input_data[i], buckets[1]['val'], buckets[1]['curr'], res)
+  print(input_data[i], buckets[5]['cur'], res)
