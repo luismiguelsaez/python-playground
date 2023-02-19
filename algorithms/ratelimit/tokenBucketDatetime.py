829bb9d5db1a1475751abe3f9023276194f1840d
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-02-19 11:30:28 +0100

    Updated algo

--- algorithms/ratelimit/tokenBucketDatetime.py
+++ algorithms/ratelimit/tokenBucketDatetime.py
@@ -8,23 +8,27 @@ buckets = {
 input_data = [
   '12:30:59', '12:30:59', '12:30:59', '12:30:59', '12:30:59', '12:30:59',
   '13:00:00', '13:00:01', '13:00:01', '13:00:01', '13:00:01', '13:00:01',
-  '13:00:03', '13:00:03', '13:00:03', '13:00:04', '13:00:04', '13:00:04',
+  '13:00:03', '13:00:03', '13:00:03', '13:00:03', '13:00:03', '13:00:03',
+  '13:00:03', '13:00:03', '13:00:03', '13:00:03', '13:00:03', '13:00:03',
+  '13:00:04', '13:00:04', '13:00:04', '13:00:04', '13:00:04', '13:00:04',
+  '13:00:05', '13:00:06', '13:00:07', '13:00:08', '13:00:09', '13:00:10',
 ]
 
+b = 1
 for i in range(len(input_data)):
-  if i == 0: buckets[1]['t_ini'] = input_data[i]
+  if i == 0: buckets[b]['t_ini'] = input_data[i]
 
-  diff = int(datetime.strptime(input_data[i],'%H:%M:%S').strftime("%s")) - int(datetime.strptime(buckets[1]['t_ini'],'%H:%M:%S').strftime("%s"))
-  if diff >= 1:
-    buckets[1]['cur'] = buckets[1]['max']
-    buckets[1]['t_ini'] = input_data[i]
+  diff = int(datetime.strptime(input_data[i],'%H:%M:%S').strftime("%s")) - int(datetime.strptime(buckets[b]['t_ini'],'%H:%M:%S').strftime("%s"))
+  if diff >= b:
+    buckets[b]['cur'] = buckets[b]['max']
+    buckets[b]['t_ini'] = input_data[i]
   
-  buckets[1]['cur'] -= 1
+  buckets[b]['cur'] -= 1
 
-  if buckets[1]['cur'] < 0:
-    res = f'drop bucket {1} execeeded'
+  if buckets[b]['cur'] < 0:
+    res = f'drop bucket {b} execeeded'
     #break
   else:
     res = 'forward'
 
-  print(f"{input_data[i],buckets[1]['cur'], res}")
+  print(f"{input_data[i],buckets[b]['cur'], res}")
