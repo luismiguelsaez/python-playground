655564ad129dccc98d80ffe9d9f77be936e268e7
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-02-23 18:29:00 +0100

    Added list function

--- api/fastapi/src/main.py
+++ api/fastapi/src/main.py
@@ -1,6 +1,7 @@
+from ctypes import addressof
 from fastapi import FastAPI, Request
 from os import environ as os_env
-from sqlalchemy import create_engine, insert
+from sqlalchemy import create_engine, insert, select, func
 from sqlalchemy.orm import sessionmaker
 from db.create import init as db_init
 
@@ -21,17 +22,24 @@ app = FastAPI()
 
 @app.get("/")
 def insert_ip(request: Request):
-    client_ip = request.client.host
-    stmt = (
-      insert(ips_table).values(address=client_ip)
-    )
-    try:
-      session.execute(stmt)
-      session.commit()
-    except Exception as insert_exc:
-      print(f'Error while inserting row: {insert_exc}')
-    return {"client_ip": client_ip}
+  client_ip = request.client.host
+  stmt = (
+    insert(ips_table)
+    .values(address=client_ip)
+  )
+  try:
+    session.execute(stmt)
+    session.commit()
+  except Exception as insert_exc:
+    print(f'Error while inserting row: {insert_exc}')
+  return {"client_ip": client_ip}
 
 @app.get("/list")
 def list_ip():
-  result = session.query(Students.first_name)
+  stmt = (
+    select([ips_table.c.address, func.count(ips_table.c.id)]).group_by(ips_table.c.address)
+  )
+
+  rows = session.execute(stmt).fetchall()
+
+  return {"ips": [ip['address'] for ip in rows]}
