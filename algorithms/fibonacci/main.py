f40c9cb4ccb94c18d24b568ed25294e0ad538f04
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-06-23 13:22:44 +0200

    Added loop

--- algorithms/fibonacci/main.py
+++ algorithms/fibonacci/main.py
@@ -1,12 +1,10 @@
 
 def fib(n: int, memo: dict = {}) -> int:
-  if n <= 2:
-    return 1
-  else:
-    if n in memo:
-      return memo[n]
-    else:
-      memo[n] = fib(n-1, memo) + fib(n-2, memo)
-      return memo[n]
+  if n <= 2: return 1
+  if n in memo: return memo[n]
 
-print(fib(55))
+  memo[n] = fib(n-1, memo) + fib(n-2, memo)
+  return memo[n]
+
+for i in range(1,20):
+  print(fib(i))
