476ca34ccdfd84f8015c0d719170f69d7bcdaeb0
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-08-13 09:51:12 +0200

    Added key_pair

--- iac/pulumi/eks/__main__.py
+++ iac/pulumi/eks/__main__.py
@@ -2,9 +2,12 @@
 
 import pulumi
 from pulumi_aws import eks, ec2, iam, get_availability_zones
+from os import environ
 
 eks_priv_subnets_num = 3
 eks_priv_subnets = []
+eks_pub_subnets_num = 3
+eks_pub_subnets = []
 
 region_azs = get_availability_zones(state="available")
 
@@ -17,6 +20,7 @@ eks_vpc = ec2.Vpc(
   }
 )
 
+# Create AWS VPC Private Subnets
 for s in range(eks_priv_subnets_num):
   eks_priv_subnets.append(
     ec2.Subnet(
@@ -27,6 +31,23 @@ for s in range(eks_priv_subnets_num):
       tags={
         "Name": f"eks-priv-subnet-{s}",
         "kubernetes.io/role/internal-elb": "1",
+        "az": region_azs.names[s],
+        "karpenter.sh/cluster": "true"
+      }
+    )
+  )
+
+# Create AWS VPC Public Subnets
+for s in range(eks_pub_subnets_num):
+  eks_pub_subnets.append(
+    ec2.Subnet(
+      f'eks-pub-subnet-{s}',
+      vpc_id=eks_vpc.id,
+      cidr_block=f'172.18.{s}.0/24',
+      availability_zone=region_azs.names[s],
+      tags={
+        "Name": f"eks-pupb-subnet-{s}",
+        "kubernetes.io/role/elb": "1",
         "az": region_azs.names[s],
         "karpenter.sh/cluster": "true"
       }
@@ -110,6 +131,7 @@ eks_cluster = eks.Cluster(
   role_arn=eks_iam_role.arn,
   vpc_config={
     "subnet_ids": [ s.id for s in eks_priv_subnets ],
+    "security_group_ids": [ eks_security_group.id ],
   },
   enabled_cluster_log_types=[
     "api",
@@ -123,6 +145,15 @@ eks_cluster = eks.Cluster(
   }
 )
 
+# Get SSH public key from home directory
+with open(f'{environ.get("HOME")}/.ssh/id_rsa.pub', 'r') as f:
+  ssh_public_key = f.read()
+
+eks_cluster_nodegroup_key_pair = ec2.KeyPair(
+  "eks-cluster-nodegroup-key-pair",
+  public_key=ssh_public_key
+)
+
 eks_cluster_nodegroup = eks.NodeGroup(
   "eks-cluster-nodegroup",
   cluster_name=eks_cluster.name,
@@ -134,9 +165,14 @@ eks_cluster_nodegroup = eks.NodeGroup(
     "max_size": 3,
     "min_size": 3,
   },
-  instance_types=["t3.medium"],
+  instance_types=[ ec2.InstanceType("t3.medium") ],
   ami_type="AL2_x86_64",
+  capacity_type="SPOT",
+  remote_access={
+    "ec2_ssh_key": eks_cluster_nodegroup_key_pair.key_name,
+  }
 )
 
-pulumi.export('eks_cluster_name', eks_cluster.name)
 pulumi.export('eks_vpc_id', eks_vpc.id)
+pulumi.export('eks_priv_subnets', [ s.id for s in eks_priv_subnets ])
+pulumi.export('eks_cluster_name', eks_cluster.name)
