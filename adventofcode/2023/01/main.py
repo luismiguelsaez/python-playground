d63f15a19023543594c629af47bd167fd44a32d7
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-12-01 18:48:26 +0100

    AoC day 1 solution part 2

--- adventofcode/2023/01/main.py
+++ adventofcode/2023/01/main.py
@@ -21,7 +21,7 @@ print(f"Part one: {sum}")
 with open('input.txt', 'r') as f:
     lines = [ l.rstrip() for l in f.readlines() ]
 
-nums = {
+nums_conv = {
     'nine':  '9',
     'eight': '8',
     'seven': '7',
@@ -33,26 +33,41 @@ nums = {
     'one':   '1'
 }
 
-def replace_nums(line: str)->str:
-    words = re.findall("(one|two|three|four|five|six|seven|eight|nine)", line)
-    for word in words:
-        line = line.replace(word, nums[word], 1)
-    return line
+nums = ['1','2','3','4','5','6','7','8','9',
+        'one','two','three','four','five','six','seven','eight','nine']
+
+def get_digits(line: str):
+    regex_str = f"({'|'.join(nums)})"
+    matches = []
+    for n in nums:
+        res = re.finditer(n, line)
+        for i in res:
+            matches.append(i.span())
+    matches.sort(key=lambda n: n[0])
+
+    digits = []
+    for m in matches:
+        digit = line[m[0]:m[1]]
+        if digit.isdigit():
+            digits.append(digit)
+        else:
+            digits.append(nums_conv[digit])
+    return digits
 
 sum = 0
+c = 0
 for line in lines:
     orig = line
-    line = replace_nums(line)
+    digits = get_digits(line)
     first = ""
     last = ""
     num = ""
-    for char in line:
-        if char.isdigit():
-            if first == "":
-                first = char
-            last = char
+    for char in digits:
+        if first == "":
+            first = char
+        last = char
     num = first + last
     sum += int(num)
-    print(f"{orig} - {line} - {first} - {last} - {num} - {sum}")
+    c += 1
 
 print(f"Part two: {sum}")
