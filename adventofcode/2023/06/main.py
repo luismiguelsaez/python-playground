0a2f4203669e1335ba65a961ca6fd52d38e09356
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-12-06 11:37:07 +0100

    Refactor func

--- adventofcode/2023/06/main.py
+++ adventofcode/2023/06/main.py
@@ -7,15 +7,9 @@ vals = []
 for time, distance in zip(re.findall('\\s+([0-9]+)', lines[0].split(':')[1]), re.findall('\\s+([0-9]+)', lines[1].split(':')[1])):
     vals.append(len(list(map(lambda x: [ i for i in range(0, int(x[0])+1) if (int(x[0]) - i) * i > int(x[1])] , [(time, distance)]))[0]))
 
-total = 0
-for i in range(len(vals)-1):
-    if i == 0:
-        total = vals[i] * vals[i+1]
-    else:
-        total = total * vals[i+1]
+def m(vals: list): return m([vals[0]*vals[1]] + vals[2:]) if len(vals) > 1 else vals[0]
 
-print(f"Part one: {total}")
-
+print(f"Part one: {m(vals)}")
 
 time, distance = ''.join(re.findall('[0-9]', lines[0].split(':')[1])), ''.join(re.findall('[0-9]', lines[1].split(':')[1]))
 ways = [ i for i in range(0, int(time)+1) if (int(time) - i) * i > int(distance) ]
