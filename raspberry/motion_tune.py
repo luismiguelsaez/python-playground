2a09ea29553e48a86f9bee55e9614f6bdb2d06bd
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2024-04-08 14:50:52 +0200

    Added video capture

--- raspberry/motion_tune.py
+++ raspberry/motion_tune.py
@@ -3,6 +3,8 @@ from gpiozero.tones import Tone
 from time import sleep
 import logging
 from sys import stdout
+import subprocess
+from datetime import datetime
 
 # GPIO configuration
 buzzer_pin = 2
@@ -26,22 +28,44 @@ logger.debug(f"Initializing objects: buzzer({buzzer_pin}), motionsensor({motion_
 buzzer = TonalBuzzer(pin=buzzer_pin)
 motion = MotionSensor(pin=motion_pin, queue_len=motion_queue_length, sample_rate=motion_sample_rate, threshold=motion_threshold)
 
-tune = [('C#4', 0.2), ('D4', 0.2), (None, 0.2),
-    ('Eb4', 0.2), ('E4', 0.2), (None, 0.6),
-    ('F#4', 0.2), ('G4', 0.2), (None, 0.6),
-    ('Eb4', 0.2), ('E4', 0.2), (None, 0.2),
-    ('F#4', 0.2), ('G4', 0.2), (None, 0.2),
-    ('C4', 0.2), ('B4', 0.2), (None, 0.2),
-    ('F#4', 0.2), ('G4', 0.2), (None, 0.2),
-    ('B4', 0.2), ('Bb4', 0.5), (None, 0.6),
-    ('A4', 0.2), ('G4', 0.2), ('E4', 0.2),
-    ('D4', 0.2), ('E4', 0.2)]
+tune =  [
+            ('C#4', 0.2), ('D4', 0.2), (None, 0.2),
+            ('Eb4', 0.2), ('E4', 0.2), (None, 0.6),
+            ('F#4', 0.2), ('G4', 0.2), (None, 0.6),
+            ('Eb4', 0.2), ('E4', 0.2), (None, 0.2),
+            ('F#4', 0.2), ('G4', 0.2), (None, 0.2),
+            ('C4', 0.2), ('B4', 0.2), (None, 0.2),
+            ('F#4', 0.2), ('G4', 0.2), (None, 0.2),
+            ('B4', 0.2), ('Bb4', 0.5), (None, 0.6),
+            ('A4', 0.2), ('G4', 0.2), ('E4', 0.2),
+            ('D4', 0.2), ('E4', 0.2)
+        ]
 
 def play_tune(tune: list = [])->None:
     for note, duration in tune:
         buzzer.play(note)
         sleep(float(duration))
     buzzer.stop()
+
+def capture_video(timeout_ms: int = 5000):
+    ts = datetime.today().strftime("%Y%m%d-%H%M%S")
+    file_name = f"/tmp/{ts}"
+    file_ext = "h264"
+    file_ext_conv = "mp4"
+
+    logger.debug(f"Capturing video: {file_name}.{file_ext}")
+    cmd_capture = subprocess.run(["libcamera-vid", f"--output={file_name}.{file_ext}", f"--timeout={timeout_ms}", "--mode=640:480"], capture_output=True)
+    if cmd_capture.returncode == 0:
+        logger.debug(f"Captured video: {file_name}.{file_ext}")
+    else:
+        logger.error(f"Error capturing video [{cmd_capture.returncode}]: {cmd_capture.stdout.decode()}")
+
+    logger.debug(f"Converting video: {file_name}.{file_ext} > {file_name}.{file_ext_conv}")
+    cmd_convert = subprocess.run(["MP4Box", "-add", f"{file_name}.{file_ext}", f"{file_name}.{file_ext_conv}"], capture_output=True)
+    if cmd_convert.returncode == 0:
+        logger.debug(f"Converted video: {file_name}.{file_ext} > {file_name}.{file_ext_conv}")
+    else:
+        logger.error(f"Error converting video [{cmd_convert.returncode}]: {cmd_convert.stdout.decode()}")
 
 while True:
     if motion.motion_detected:
