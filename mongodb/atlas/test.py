734b43170a255f4ab253008631e1ce461bca5cde
luismiguelsaez <luismiguelsaez@protonmail.com>
2024-12-23 19:04:20 +0100

    Added test

--- mongodb/atlas/test.py
+++ mongodb/atlas/test.py
@@ -2,16 +2,36 @@ import asyncio
 import httpx
 from sys import argv
 
-class Atlas():
+class Admin():
     __api_endpoint = 'https://cloud.mongodb.com'
     __api_base_path = '/api/atlas/v2'
-    __api_appservices_endpoint = 'https://services.cloud.mongodb.com'
-    __api_appservices_base_path = '/api/admin/v3.0'
     __api_headers = {
         'Accept': 'application/vnd.atlas.2024-08-05+json',
         'Content-type': 'application/vnd.atlas.2024-08-05+json'
     }
-    _api_appservices_headers = {
+
+    def __init__(self, public_key: str, private_key: str, project_id: str):
+        self.__public_key = public_key
+        self.__private_key = private_key
+        self.__project_id = project_id
+
+    # Admin API methods
+    async def get_clusters(self) -> tuple[bool, dict]:
+        async with httpx.AsyncClient() as client:
+            r = await client.get(
+                url=f'{self.__api_endpoint}{self.__api_base_path}/groups/{self.__project_id}/clusters',
+                auth=httpx.DigestAuth(self.__public_key, self.__private_key),
+                headers=self.__api_headers
+            )
+        if r.status_code == 200:
+            return True, r.json()
+        else:
+            return False, { 'code': r.status_code, 'message': r.text }
+
+class Appservices():
+    __api_appservices_endpoint = 'https://services.cloud.mongodb.com'
+    __api_appservices_base_path = '/api/admin/v3.0'
+    __api_appservices_headers = {
         "Accept": "application/json",
         "Content-Type": "application/json"
     }
@@ -23,18 +43,8 @@ class Atlas():
         self.__appservices_token = None
         self.__appservices_refresh_token = None
 
-    # Admin API methods
-    async def get_clusters(self):
-        async with httpx.AsyncClient() as client:
-            r = await client.get(
-                url=f'{self.__api_endpoint}{self.__api_base_path}/groups/{self.__project_id}/clusters',
-                auth=httpx.DigestAuth(self.__public_key, self.__private_key),
-                headers=self.__api_headers
-            )
-            return r.json()
-
     # Appservices API methods
-    async def get_appservices_apps(self):
+    async def get_apps(self) -> tuple[bool, dict]:
         await self.get_appservices_token()
         async with httpx.AsyncClient() as client:
             r = await client.get(
@@ -43,9 +53,26 @@ class Atlas():
                     'Authorization': f'Bearer {self.__appservices_token}'
                 }
             )
-            return r.json()
+        if r.status_code == 200:
+            return True, r.json()
+        else:
+            return False, { 'code': r.status_code, 'message': r.text }
 
-    async def get_appservices_token(self):
+    async def get_functions(self, app_id: str) -> tuple[bool, dict]:
+        await self.get_appservices_token()
+        async with httpx.AsyncClient() as client:
+            r = await client.get(
+                url=f'{self.__api_appservices_endpoint}{self.__api_appservices_base_path}/groups/{self.__project_id}/apps/{app_id}/functions',
+                headers={
+                    'Authorization': f'Bearer {self.__appservices_token}'
+                }
+            )
+        if r.status_code == 200:
+            return True, r.json()
+        else:
+            return False, { 'code': r.status_code, 'message': r.text }
+
+    async def get_appservices_token(self) -> None:
         async with httpx.AsyncClient() as client:
             if self.__appservices_token:
                 r = await client.post(
@@ -70,16 +97,24 @@ class Atlas():
 
 
 async def main():
-    atlas = Atlas(
+    atlas_admin = Admin(
             public_key=argv[1],
             private_key=argv[2],
             project_id=argv[3]
     )
 
-    clusters = await atlas.get_clusters()
-    apps = await atlas.get_appservices_apps()
-    print(f"Clusters: {clusters}")
-    print(f"Apps: {apps}")
+    atlas_appservices = Appservices(
+            public_key=argv[1],
+            private_key=argv[2],
+            project_id=argv[3]
+    )
+
+    res_clusters, out_clusters = await atlas_admin.get_clusters()
+    res_apps, out_apps = await atlas_appservices.get_apps()
+    res_functions, out_functions = await atlas_appservices.get_functions(out_apps[0]["_id"])
+    print(f"Clusters: {out_clusters}")
+    print(f"Apps: {out_apps}")
+    print(f"Functions: {out_functions}")
 
 if __name__ == '__main__':
     asyncio.run(main())
